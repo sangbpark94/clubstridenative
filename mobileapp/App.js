913f3834a6ff19ea9635b6ee5fa4260ea4d1016c
wvazquez <vazquezwilmer@gmail.com>
2018-05-08 10:27:16 -0400

    Removes credentials.

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as firebase from "firebase";
 import {AppLoading} from 'expo';
-import Navigator from './navigation/Navigator'
-import LoginScreen from './pages/LoginScreen'
+import Navigator from './navigation/Navigator';
+import LoginScreen from './pages/LoginScreen';
+import config from './config';
 export default class App extends Component {
 
   constructor(props){
@@ -19,16 +20,7 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    var config = {
-      apiKey: "AIzaSyBiViir44Rl253QD8PgkXhrj5Ccz87qjk0",
-      authDomain: "clubstride-36353.firebaseapp.com",
-      databaseURL: "https://clubstride-36353.firebaseio.com",
-      projectId: "clubstride-36353",
-      storageBucket: "clubstride-36353.appspot.com",
-      messagingSenderId: "461351295355"
-    };
     firebase.initializeApp(config);
-
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         if (this.state.new_user){
